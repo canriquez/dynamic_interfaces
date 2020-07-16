ac7cc1fd7fc39e976f3fb2cf49eb5b24fa873fcf
Carlos  Anriquez <canriquez@gmail.com>
2020-07-16 19:36:28 -0300

    Initial commit

---
+++ src/components/navigation.js
@@ -0,0 +1,62 @@
+
+import navIcon from '../images/burger.svg';
+import xNavIcon from '../images/xicon.svg';
+import brandLogo from '../images/dp_logo.svg';
+import globalLogo from '../images/globe.svg';
+
+
+function renderNavCartItemNumber(items) {
+  document.getElementById('navItems').innerHTML = items;
+}
+
+function renderNavigation() {
+  let htmlTag = '<header id="navigation">';
+  htmlTag += '<nav class="nav-wrap row-flex">';
+  htmlTag += '<div class="left-wrap row-flex">';
+  htmlTag += '<div id="burger-menu"></div>';
+  htmlTag += '<div id="x-mobile-menu" class="hide"></div>';
+  htmlTag += '<div id="brand-nav"></div></div>';
+  htmlTag += '<div id="action-wrap" class="action-wrap row-flex">';
+  htmlTag += '<p id="m1">home</p>';
+  htmlTag += '<p id="m2">our menu</p>';
+  htmlTag += '<p id="m3" class="row-flex">your cart<span id="navItems">2</span></p>';
+  htmlTag += '<p id="m4" class="row-flex">contact us</p>';
+  htmlTag += '<div id="global"></div></div></nav></header>';
+  htmlTag += '<section id="main"></section>';
+
+  document.getElementById('content').innerHTML = htmlTag;
+  // Add Navigation Icons
+
+  const navMenuIcon = new Image();
+  const xMobileMenuIcon = new Image();
+  const navBrand = new Image();
+  const navGlobal = new Image();
+  navMenuIcon.src = navIcon;
+  xMobileMenuIcon.src = xNavIcon;
+  navBrand.src = brandLogo;
+  navGlobal.src = globalLogo;
+  document.getElementById('burger-menu').appendChild(navMenuIcon);
+  document.getElementById('x-mobile-menu').appendChild(xMobileMenuIcon);
+  document.getElementById('brand-nav').appendChild(navBrand);
+  document.getElementById('global').appendChild(navGlobal);
+
+  renderNavCartItemNumber(2);
+}
+
+function clearNavigation() {
+  document.getElementById('navigation').innerHTML = '';
+}
+
+function hideNavigation() {
+  document.getElementById('navigation').classList.add('hide');
+  document.getElementById('navigation').classList.remove('show');
+}
+
+function showNavigation() {
+  document.getElementById('navigation').classList.add('show');
+  document.getElementById('navigation').classList.remove('hide');
+}
+
+export {
+  renderNavigation, clearNavigation, hideNavigation, showNavigation,
+};
---
+++ src/index.js
@@ -0,0 +1,133 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+/* eslint-disable no-use-before-define */
+
+import './style.css';
+import { clearAllSections } from './components/genericrender';
+import {
+  renderNavigation, clearNavigation, hideNavigation, showNavigation,
+} from './components/navigation';
+import {
+  renderMainSection, clearMainSection, hideMainSection, showMainSection,
+} from './components/mainsection';
+import {
+  renderMenuSection, clearMenuSection, hideMenuSection, showMenuSection,
+} from './components/menusection';
+import {
+  renderCartSection, clearCartSection, hideCartSection, showCartSection,
+} from './components/cartsection';
+import {
+  renderContactSection, clearContactSection, hideContactSection, showContactSection,
+} from './components/contactsection';
+import {
+  renderFooterSection, clearFooterSection, hideFooterSection, showFooterSection,
+} from './components/footersection';
+
+
+function updateNavigationFunctions() {
+  if (window.screen.width < 768) {
+    document.getElementById('action-wrap').classList.add('hide');
+    document.getElementById('action-wrap').classList.remove('show');
+    document.getElementById('burger-menu').classList.add('show');
+    document.getElementById('burger-menu').classList.remove('hide');
+  } else {
+    document.getElementById('burger-menu').classList.add('hide');
+    document.getElementById('action-wrap').classList.remove('show');
+    document.getElementById('action-wrap').classList.remove('hide');
+    document.getElementById('action-wrap').classList.add('show');
+  }
+}
+
+function pageLoad() {
+  renderNavigation();
+  updateNavigationFunctions();
+  addListeners();
+  renderMainSection();
+  renderMenuSection();
+  renderCartSection();
+  renderContactSection();
+  renderFooterSection();
+}
+
+// tab-switching logic
+
+function showMobileMenu() {
+  document.getElementById('action-wrap').classList.remove('hide');
+  document.getElementById('action-wrap').classList.add('show');
+  document.getElementById('burger-menu').classList.remove('show');
+  document.getElementById('burger-menu').classList.add('hide');
+  document.getElementById('x-mobile-menu').classList.remove('hide');
+  document.getElementById('x-mobile-menu').classList.add('show');
+}
+
+function hideMobileMenu() {
+  document.getElementById('action-wrap').classList.remove('show');
+  document.getElementById('action-wrap').classList.add('hide');
+  document.getElementById('burger-menu').classList.remove('hide');
+  document.getElementById('burger-menu').classList.add('show');
+  document.getElementById('x-mobile-menu').classList.remove('show');
+  document.getElementById('x-mobile-menu').classList.add('hide');
+}
+
+function home() {
+  clearAllSections();
+  pageLoad();
+}
+
+function menu() {
+  clearAllSections();
+  renderNavigation();
+  updateNavigationFunctions();
+  addListeners();
+  renderMenuSection();
+  renderFooterSection();
+}
+
+function cart() {
+  clearAllSections();
+  renderNavigation();
+  updateNavigationFunctions();
+  addListeners();
+  renderCartSection();
+  renderFooterSection();
+}
+
+function contact() {
+  clearAllSections();
+  renderNavigation();
+  updateNavigationFunctions();
+  addListeners();
+  renderContactSection();
+  renderFooterSection();
+}
+
+function addListeners() {
+  // add event listeners to menu options on each render
+  document.getElementById('m1').addEventListener('click', () => {
+    home();
+  });
+  document.getElementById('m2').addEventListener('click', () => {
+    menu();
+  });
+  document.getElementById('m3').addEventListener('click', () => {
+    cart();
+  });
+  document.getElementById('m4').addEventListener('click', () => {
+    contact();
+  });
+  document.getElementById('burger-menu').addEventListener('click', () => {
+    showMobileMenu();
+  });
+  document.getElementById('x-mobile-menu').addEventListener('click', () => {
+    hideMobileMenu();
+  });
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  pageLoad();
+
+  window.addEventListener('resize', () => {
+    updateNavigationFunctions();
+  });
+});
