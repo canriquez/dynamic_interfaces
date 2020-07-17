19137fcbdfac252aed9413d294ec9a05f123d752
Carlos Anriquez <canriquez@gmail.com>
2020-07-17 14:53:07 -0300

    animating the background image

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 
 
+import './styles/header.css'
+import './styles/main.css'
+import rockSvg from './images/01/rocky-dashed.svg'
+
+
+let image = document.getElementById("rocky-dashed")
+image.src = rockSvg;
+
 
 document.addEventListener('DOMContentLoaded', () => {
 
