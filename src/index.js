025e3a4bd2ee0a64e5c50f542ad2eadda8831d7c
Carlos Anriquez <canriquez@gmail.com>
2020-07-17 16:04:12 -0300

    add a scroll cue

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
 
-import './styles/header.css'
-import './styles/main.css'
-import rockSvg from './images/01/rocky-dashed.svg'
+import './styles/header.css';
+import './styles/main.css';
+import rockSvg from './images/01/rocky-dashed.svg';
+import downArrow from './images/01/downarrow.png';
 
 
 let image = document.getElementById("rocky-dashed")
 image.src = rockSvg;
+
+let arrowTag = document.getElementById('downarrow')
+arrowTag.src = downArrow;
 
 
 document.addEventListener('DOMContentLoaded', () => {
